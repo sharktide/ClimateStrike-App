f2089a47f9ee1cd5496a76a24752a0a35ceb86bc
sharktide <meherrihaan@gmail.com>
2025-09-12 14:04:05 -0400

    Change display name

--- STRIKE/App.xaml.cs
+++ STRIKE/App.xaml.cs
@@ -9,7 +9,7 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new MainPage()) { Title = "STRIKE" };
+            return new Window(new MainPage()) { Title = "Strike AI 2025" };
         }
     }
 }
