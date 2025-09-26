c0cbb13be3f440c977d9887447155001b70818de
sharktide <sharktidedev@gmail.com>
2025-09-26 16:47:00 -0400

    Installers, Workflows for Windows

---
+++ pcbuild.bat
@@ -0,0 +1,3 @@
+dotnet publish STRIKE/StrikeAI.csproj -f net9.0-windows10.0.19041.0
+tar -a -c -f "redist/ClimateStrike_AI-x64-redist.zip" "./STRIKE/bin/Release/net9.0-windows10.0.19041.0"
+iscc setup.iss
